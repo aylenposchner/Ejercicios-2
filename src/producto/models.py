0ed1bf3f43cc7e324ec51fc6d7233bf120b1df1e
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-09 22:32:35 -0300

    Fin 09.12

--- src/producto/models.py
+++ src/producto/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=15, unique=True)
+    # descripcion = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return self.nombre
+    class Meta:
+        verbose_name = "Categoría de Produecto"
+        verbose_name_plural = "Categorías de Productos"
