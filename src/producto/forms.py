e0680ceff1ab9ee86aef4331e4c14d7188885686
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-09 22:13:54 -0300

    feat: Validaciones en Categoría

--- src/producto/forms.py
+++ src/producto/forms.py
@@ -5,3 +5,10 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = models.Categoria
         fields = ["nombre"]
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre","")
+        if len(nombre)<3:
+            raise forms.ValidationError("La longitud debe ser mayor a 3 caracteres")
+        if not nombre.isalpha():
+            raise forms.ValidationError("La categoría no puede contener numeros")
+        return nombre
