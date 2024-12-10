ccdf5a1c94e2f0f6ed1ba15f0c10632f74809433
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-09 22:08:41 -0300

    feat: Formulario Categorias

--- src/producto/models.py
+++ src/producto/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return self.nombre
--- src/producto/views.py
+++ src/producto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models, forms
 
 def index(request):
@@ -9,5 +9,12 @@ def Categoria_list(request):
     return render(request,"producto/categoria_list.html",{"categorias":categorias})
 
 def Categoria_create(request):
-    form = forms.CategoriaForm()
+    if request.method == "GET":
+        form = forms.CategoriaForm()
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
+
     return render(request,"producto/categoria_form.html",{"form":form})
